ae09562e97396526085bb6477765671c5e9a60ef
Mehedi-2211 <hassanmehedi2211@gmail.com>
2023-08-29 18:56:25 +0600

    my first java program

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+//		System.out.println("Hello world");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Md. Mehedi Hassan");
+
+	}
+
+}
